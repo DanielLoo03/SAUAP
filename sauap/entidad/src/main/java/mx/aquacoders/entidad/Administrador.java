bd3f84abe79cc97558cdd43fe53da1bc895ffbf2
DanielLoo03 <dloog23@gmail.com>
2025-03-14 18:56:51 -0700

    Creación de capa de entidad
    Se crea un nuevo módulo (entidad), se crea una conexión con la base de datos y se generan cuatro modelos referentes a las tablas de la base de datos.

---
+++ sauap/entidad/src/main/java/mx/aquacoders/entidad/Administrador.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.aquacoders.entidad;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author danie
+ */
+@Entity
+@Table(name = "administrador")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Administrador.findAll", query = "SELECT a FROM Administrador a")
+    , @NamedQuery(name = "Administrador.findByIdUsuario", query = "SELECT a FROM Administrador a WHERE a.idUsuario = :idUsuario")})
+public class Administrador implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_usuario")
+    private Integer idUsuario;
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Usuario usuario;
+
+    public Administrador() {
+    }
+
+    public Administrador(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUsuario != null ? idUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Administrador)) {
+            return false;
+        }
+        Administrador other = (Administrador) object;
+        if ((this.idUsuario == null && other.idUsuario != null) || (this.idUsuario != null && !this.idUsuario.equals(other.idUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.aquacoders.entidad.Administrador[ idUsuario=" + idUsuario + " ]";
+    }
+    
+}
